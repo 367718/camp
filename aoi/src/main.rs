886dbf3a1f934541eba41ca2ed1435e6993db738
367718 <367718@users.noreply.github.com>
2026-06-15 11:56:03 -0300

    placeholder

--- aoi/src/main.rs
+++ aoi/src/main.rs
@@ -85,95 +85,82 @@ fn handle_request(request: &mut Request) -> Result<(), Box<dyn Error>> {
     let endpoint = request.endpoint()
         .ok_or("Invalid request")?;
     
-    // -------------------- index --------------------
-    
-    if endpoint == b"GET /" {
+    match endpoint {
         
-        request.start_response(StatusCode::Ok, ContentType::Html, CacheControl::Static)
-            .and_then(|mut response| response.write_all(INDEX))?;
+        // -------------------- general --------------------
         
-        return Ok(());
+        b"GET /" => index(request),
+        b"GET /styles.css" => styles(request),
+        b"GET /scripts.js" => scripts(request),
         
-    }
-    
-    // -------------------- styles --------------------
-    
-    if endpoint == b"GET /styles.css" {
+        // -------------------- commands --------------------
         
-        request.start_response(StatusCode::Ok, ContentType::Css, CacheControl::Static)
-            .and_then(|mut response| response.write_all(STYLES))?;
+        b"POST /play" => send_command(request, COMMAND_PLAY),
+        b"POST /minuschapter" => send_command(request, COMMAND_MINUSCHAPTER),
+        b"POST /pluschapter" => send_command(request, COMMAND_PLUSCHAPTER),
+        b"POST /minusplaylist" => send_command(request, COMMAND_MINUSPLAYLIST),
+        b"POST /plusplaylist" => send_command(request, COMMAND_PLUSPLAYLIST),
+        b"POST /minus5" => send_command(request, COMMAND_MINUS5),
+        b"POST /plus5" => send_command(request, COMMAND_PLUS5),
+        b"POST /minus75" => send_command(request, COMMAND_MINUS75),
+        b"POST /plus75" => send_command(request, COMMAND_PLUS75),
+        b"POST /fullscreen" => send_command(request, COMMAND_FULLSCREEN),
+        b"POST /subtitles" => send_command(request, COMMAND_SUBTITLES),
+        b"POST /title" => send_command(request, COMMAND_TITLE),
+        b"POST /time" => send_command(request, COMMAND_TIME),
+        b"POST /quit" => send_command(request, COMMAND_QUIT),
         
-        return Ok(());
+        // -------------------- not found --------------------
         
-    }
-    
-    // -------------------- scripts --------------------
-    
-    if endpoint == b"GET /scripts.js" {
-        
-        request.start_response(StatusCode::Ok, ContentType::Javascript, CacheControl::Static)
-            .and_then(|mut response| response.write_all(SCRIPTS))?;
-        
-        return Ok(());
+        _ => Err("Endpoint not found".into()),
         
     }
-    
-    // -------------------- command --------------------
-    
-    if let Some(command) = get_command(endpoint) {
-        
-        let pipe = rin::get::<&str>(b"pipe")?;
-        write_to_named_pipe(pipe, command)?;
-        request.start_response(StatusCode::Ok, ContentType::Plain, CacheControl::Dynamic)?;
-        
-        return Ok(());
-        
-    }
-    
-    // -------------------- not found --------------------
-    
-    Err("Endpoint not found".into())
 }
 
-fn get_command(endpoint: &[u8]) -> Option<&'static [u8]> {
-    let result: &[u8] = match endpoint {
-        b"POST /play" => COMMAND_PLAY,
-        b"POST /minuschapter" => COMMAND_MINUSCHAPTER,
-        b"POST /pluschapter" => COMMAND_PLUSCHAPTER,
-        b"POST /minusplaylist" => COMMAND_MINUSPLAYLIST,
-        b"POST /plusplaylist" => COMMAND_PLUSPLAYLIST,
-        b"POST /minus5" => COMMAND_MINUS5,
-        b"POST /plus5" => COMMAND_PLUS5,
-        b"POST /minus75" => COMMAND_MINUS75,
-        b"POST /plus75" => COMMAND_PLUS75,
-        b"POST /fullscreen" => COMMAND_FULLSCREEN,
-        b"POST /subtitles" => COMMAND_SUBTITLES,
-        b"POST /title" => COMMAND_TITLE,
-        b"POST /time" => COMMAND_TIME,
-        b"POST /quit" => COMMAND_QUIT,
-        _ => return None,
-    };
+fn index(request: &mut Request) -> Result<(), Box<dyn Error>> {
+    request.start_response(StatusCode::Ok, ContentType::Html, CacheControl::Static)
+        .and_then(|mut response| response.write_all(INDEX))?;
     
-    Some(result)
+    Ok(())
 }
 
-fn write_to_named_pipe(path: &str, data: &[u8]) -> io::Result<()> {
+fn styles(request: &mut Request) -> Result<(), Box<dyn Error>> {
+    request.start_response(StatusCode::Ok, ContentType::Css, CacheControl::Static)
+        .and_then(|mut response| response.write_all(STYLES))?;
+    
+    Ok(())
+}
+
+fn scripts(request: &mut Request) -> Result<(), Box<dyn Error>> {
+    request.start_response(StatusCode::Ok, ContentType::Javascript, CacheControl::Static)
+        .and_then(|mut response| response.write_all(SCRIPTS))?;
+    
+    Ok(())
+}
+
+fn send_command(request: &mut Request, command: &[u8]) -> Result<(), Box<dyn Error>> {
+    let pipe = rin::get::<&str>(b"pipe")?;
+    
     unsafe {
         
         let result = WaitNamedPipeW(
-            chikuwa::win_string(path).as_ptr(),
+            chikuwa::win_string(pipe).as_ptr(),
             PIPE_MAX_WAIT_AS_MILLIS,
         );
         
         if result == 0 {
-            return Err(io::Error::last_os_error());
+            return Err(Box::new(io::Error::last_os_error()));
         }
         
     }
     
-    let mut pipe = File::options()
+    let mut file = File::options()
         .write(true)
-        .open(path)?;
+        .open(pipe)?;
     
-    pipe.write_all(data)
+    file.write_all(command)?;
+    
+    request.start_response(StatusCode::Ok, ContentType::Plain, CacheControl::Dynamic)?;
+    
+    Ok(())
 }
